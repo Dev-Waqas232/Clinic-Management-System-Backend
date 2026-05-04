857de446011b5c732a538a6ca000b15bd7ddaccb
Dev-Waqas232 <devwaqas232@gmail.com>
2026-05-04 14:44:54 +0500

    feat: added env config

---
+++ app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    DB_URL: str
+    PORT: int = 8000
+    HOST: str = "0.0.0.0"
+
+settings = Settings() # type: ignore
