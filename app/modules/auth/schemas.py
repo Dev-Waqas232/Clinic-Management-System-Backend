bbc13a37db8cfe91c41148dd83e596ffd3c2a116
Dev-Waqas232 <devwaqas232@gmail.com>
2026-05-05 21:58:17 +0500

    feat: added register body schema

---
+++ app/modules/auth/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field, EmailStr
+
+from app.modules.users.models import Role
+
+class RegisterBody(BaseModel):
+    first_name: str = Field(examples=["Usman"], max_length=30)
+    last_name: str = Field(examples=["Ghani"], max_length=30)
+    email: EmailStr
+    password:str = Field(min_length=8)
+    role: Role
