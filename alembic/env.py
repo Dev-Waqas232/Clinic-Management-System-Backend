c0659e5614cf2a0007ad5d6e3e68eb47b74563ae
Dev-Waqas232 <devwaqas232@gmail.com>
2026-05-05 21:30:17 +0500

    feat: added migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,11 @@
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy.ext.asyncio import create_async_engine
 from app.db.base import Base
 from app.core.config import settings
+from app.db import models  # noqa: F401
+import asyncio
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -40,7 +40,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option(settings.DB_URL)
+    url = settings.DB_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -51,26 +51,19 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    async def do_run():
+        engine = create_async_engine(settings.DB_URL)
+        async with engine.connect() as conn:
+            await conn.run_sync(do_migrations)
+        await engine.dispose()
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
-
+    def do_migrations(conn):
+        context.configure(connection=conn, target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
 
+    asyncio.run(do_run())
 
 if context.is_offline_mode():
     run_migrations_offline()
---
+++ app/db/models.py
@@ -0,0 +1,2 @@
+from app.modules.auth.models import Auth
+from app.modules.users.models import User
