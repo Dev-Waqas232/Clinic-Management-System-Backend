ddd5a18e8c98c555d1c22d8768fadbff0084f1c7
Dev-Waqas232 <devwaqas232@gmail.com>
2026-06-03 17:06:09 +0500

    added ssl option to celery

--- app/core/celery.py
+++ app/core/celery.py
@@ -6,6 +6,8 @@ app = Celery(
     "clinic-ms",
     broker=f"rediss://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}",
     backend=f"rediss://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+    broker_use_ssl={"ssl_cert_reqs": "CERT_NONE"},
+    redis_backend_use_ssl={"ssl_cert_reqs": "CERT_NONE"},
 )
 
 app.autodiscover_tasks(["app.tasks.email_tasks"])
